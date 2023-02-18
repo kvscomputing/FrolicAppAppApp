80c4c444226c610396ea9ed69ad2ec728e0f180a
kvscomputing <kvseetharaman2@gmail.com>
2023-02-18 00:19:09 -0800

    Initial Commit

---
+++ FrolicAppAppApp/FrolicAppAppAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FrolicAppAppAppApp.swift
+//  FrolicAppAppApp
+//
+//  Created by Karthik Seetharaman on 2/18/23.
+//
+
+import SwiftUI
+
+@main
+struct FrolicAppAppAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
